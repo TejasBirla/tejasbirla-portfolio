1bee8399404dfb196ddd9aa5a0717725e68d0488
Tejas Birla <tejasbirla3@gmail.com>
2026-01-24 20:23:08 +0530

    Projects updated.

--- src/data.js
+++ src/data.js
@@ -3,11 +3,21 @@ import pulseCheckImg from "../src/assets/pulseCheckImg.png";
 import EventlyImg from "../src/assets/EventlyImg.png";
 import SnipVaultImg from "../src/assets/SnipVaultImg.png";
 import QuickChatImg from "../src/assets/quickChatImg.png";
-import WeatherAppImg from "../src/assets/weather.jpg";
+import QuickLinkImg from "../src/assets/quickLink-hero-img.png";
 
 const projects = [
   {
     pId: 1,
+    name: "Evently - Event Booking Platform",
+    image: EventlyImg,
+    description:
+      "An online event booking platform developed with MERN stack that allows users to browse, book, and pay for events using Cashfree integration.",
+    liveDemo: "https://evently-ticket-booking-website.vercel.app",
+    githubLink:
+      "https://github.com/TejasBirla/Evently---Ticket-Booking-website",
+  },
+  {
+    pId: 2,
     name: "pulseCheck Monitoring System",
     image: pulseCheckImg,
     description:
@@ -16,23 +26,13 @@ const projects = [
     githubLink: "https://github.com/TejasBirla/pulseCheck",
   },
   {
-    pId: 2,
+    pId: 3,
     name: "Shopper E-Commerce Platform",
     image: ShopperImg,
     description:
       "A full-stack e-commerce website built using the MERN stack with features like product browsing, cart system and Cashfree payment integration.",
     liveDemo: "https://shopper-shopping-website-frontend.vercel.app",
     githubLink: "https://github.com/TejasBirla/SHOPPER-shopping-website",
-  },
-  {
-    pId: 3,
-    name: "Evently - Event Booking Platform",
-    image: EventlyImg,
-    description:
-      "An online event booking platform developed with MERN stack that allows users to browse, book, and pay for events using Cashfree integration.",
-    liveDemo: "https://evently-ticket-booking-website.vercel.app",
-    githubLink:
-      "https://github.com/TejasBirla/Evently---Ticket-Booking-website",
   },
   {
     pId: 4,
@@ -54,12 +54,12 @@ const projects = [
   },
   {
     pId: 6,
-    name: "Weather App",
-    image: WeatherAppImg,
+    name: "QuickLink - URLs Shortner",
+    image: QuickLinkImg,
     description:
-      "A weather forecast app built with React and OpenWeather API that displays real-time weather updates of any city.",
-    liveDemo: "",
-    githubLink: "https://github.com/TejasBirla/Weather-App-using-React",
+      "Fast and secure URL shortener built with React and Node.js, allowing users to create shareable links instantly with a smooth and responsive experience.",
+    liveDemo: "https://quick-link-eta.vercel.app",
+    githubLink: "https://github.com/TejasBirla/QuickLink-URL-Shortner",
   },
 ];
 
